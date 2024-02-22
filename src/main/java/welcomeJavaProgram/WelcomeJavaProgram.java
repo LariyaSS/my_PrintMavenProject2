09b2c7e4145c4558e312384f4f7100f3522233d3
111 <111@DESKTOP-H6PUQT2>
2024-02-22 18:25:08 +0530

    This is my first commit test

---
+++ src/main/java/welcomeJavaProgram/WelcomeJavaProgram.java
@@ -0,0 +1,11 @@
+package welcomeJavaProgram;
+
+public class WelcomeJavaProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("welcome Java");
+
+	}
+
+}
